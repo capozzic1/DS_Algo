4b49c3e3c362ccd67c09ac9713b6081e48b5770e
Chris <capozzic1@gmail.com>
2018-01-18 21:25:17 +0900

    added in add method

--- bst.js
+++ bst.js
@@ -1,23 +1,45 @@
-function Node(key) {
-  this.key = key;
-  this.right = null;
-  this.left = null;
+class Node {
+  constructor(val, left = null, right = null) {
+    this.value = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-function BST() {
+var n = new Node(3);
+//console.log(n);
 
-  this.search = function(root, key) {
-    if (root.key === key || root === null) {
-      return root;
-    }
-
-    //10 - left(5,4,3) - right(12-13-14)
-    if (key < root.key) {
-      return this.search(root.left, key);
-    }
-
-    return this.search(root.right, key);
-
+class BST {
+  constructor() {
+    this.root = null;
   }
 
+  add(root, val) {
+    if (root === null) {
+      this.root = new Node(val);
+    } else {
+      if (root.value > val) {
+        if (root.left === null) {
+          root.left = new Node(val);
+        } else {
+          //console.log(root.left);
+          return this.add(root.left, val);
+        }
+      } else {
+        if (root.right === null) {
+          root.right = new Node(val);
+        } else {
+          return this.add(root.right, val);
+        }
+      }
+    }
+  }
 }
+
+let b = new BST();
+b.add(b.root, 10);
+b.add(b.root, 12);
+b.add(b.root, 80);
+b.add(b.root, 7);
+
+console.log(b.root);
