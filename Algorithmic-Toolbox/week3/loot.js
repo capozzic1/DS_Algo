2bb6f8ac70df4398d16a66dde67ee8f41a83a0bf
capozzic1 <capozzic1@gmail.com>
2018-01-30 09:34:24 +0900

    Updated solution

--- Algorithmic-Toolbox/week3/loot.js
+++ Algorithmic-Toolbox/week3/loot.js
@@ -15,14 +15,39 @@ rstream.on('data', function(buf) {
 
 function getMaxValue(arr) {
   let nWeight = arr.splice(0, 1);
-  let n = nWeight[0];
-  let weight = nWeight[1];
+  let n = nWeight[0].splice(0,1);
+  let weight = nWeight[0].splice(0,1);
+  let maxVal = 0,
+  knapsack = 0;
 
-  for (let i = 0, len = arr.length; i < len; i++) {
+
+  if (n == 1){
+
+    arr[0][2] = arr[0][0]/arr[0][1];
+    console.log(arr);
+    let vd = arr[0][2];
+    maxVal = weight * vd;
+    return maxVal;
+  //  maxVal = ;
+    //console.log(typeof arr[0][1], typeof arr[0][2])
+    //return maxVal;
+  }
+
+  for (let i = 0; i < n; i++) {
+
     arr[i][2] = arr[i][0] / arr[i][1];
   }
 
   arr.sort((a, b) => b[2] - a[2]);
 
-  return arr;
+  for(let i = 0; i < n; i++){
+
+    if (arr[i][1] < weight && knapsack < weight){
+      maxVal += arr[i][0];
+      knapsack += arr[i][1];
+    }
+  }
+
+
+  return maxVal;
 }
