f89f13f8eaf812b3aba83d6196c354d9a8059e3a
Chris <capozzic1@gmail.com>
2018-02-10 14:43:46 +0900

    Updated

--- Sorting/quicksort.js
+++ Sorting/quicksort.js
@@ -1,46 +1,86 @@
-function quickSort(arr, low, high) {
+// function quickSort(arr, low, high) {
+//   if (low < high) {
+//     let pi = partition(arr, low, high);
+//
+//     quickSort(arr, low, pi - 1);
+//     quickSort(arr, pi + 1, high);
+//
+//   }
+// }
+//
+// function partition(arr, low, high) {
+//
+//   let pivot = arr[high];
+//   let i = (low - 1)
+//   console.log(i)
+//   for (let j = low; j < high; j++) {
+//     if (arr[j] <= pivot) {
+//       i++;
+//       console.log(arr[i], arr[j])
+//
+//       swap(arr, i, j);
+//     }
+//   }
+//   swap(arr, i + 1, high);
+//
+//   return (i + 1);
+//
+// }
+//
+// function swap(arr, i, j) {
+//   let temp = arr[i];
+//   arr[i] = arr[j];
+//   arr[j] = temp;
+//
+// }
+//low is starting idx, high is ending idx
+
+function qSort(arr, low, high) {
+  console.log(arr, low, high);
   if (low < high) {
+
     let pi = partition(arr, low, high);
-
-    quickSort(arr, low, pi - 1);
-    quickSort(arr, pi + 1, high);
-
+    console.log(`${pi} pi`);
+    qSort(arr, low, pi - 1);
+    qSort(arr, pi + 1, high);
   }
 }
 
 function partition(arr, low, high) {
 
   let pivot = arr[high];
-  let i = (low - 1)
-  console.log(i)
+
+  let i = (low - 1); //smaller element
+
+  // -1 0, 0 1
   for (let j = low; j < high; j++) {
     if (arr[j] <= pivot) {
       i++;
-      console.log(arr[i], arr[j])
 
       swap(arr, i, j);
+
     }
   }
   swap(arr, i + 1, high);
 
-  return (i + 1);
-
+  return i + 1;
 }
 
 function swap(arr, i, j) {
+  //console.log(i, j)
   let temp = arr[i];
   arr[i] = arr[j];
   arr[j] = temp;
-
 }
 
 let arr = [
   1,
   5,
-  40,
-  6,
   3,
-  20
+  2,
+  200,
+  50
 ] // 1 3 5 6 20 40
-quickSort(arr, 0, arr.length - 1);
+let len = arr.length - 1;
+qSort(arr, 0, len);
 console.log(arr)
